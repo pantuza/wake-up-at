a0396f6d10a84809c727950effc989f62cd6e24d
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-03-28 13:49:13 -0300

    Adding fake data and increment on Day

--- wake-up-at.go
+++ wake-up-at.go
@@ -13,5 +13,31 @@ func main () {
 
     now := time.Now()
 
-    fmt.Printf("Hello nurse: %d\n", now.Year())
+    // Fake input data
+    in_period := "AM"
+    in_hours := 8
+    in_minutes := 15
+
+
+    // Create the time to wake
+    var hours time.Duration = time.Duration(in_hours) * time.Hour
+    var minutes time.Duration = time.Duration(in_minutes) * time.Minute
+    var wake_duration time.Duration = hours + minutes
+
+
+
+    wake_time := time.Now()
+    if in_period == "AM" {
+
+        if now.Hour() > 12 {
+
+
+            fmt.Printf("Duration time: %v\n", wake_duration)
+            wake_time = wake_time.AddDate(0, 0, 1)
+            //wake_time = wake_time.Truncate(wake_duration)
+        }
+    }
+
+    fmt.Printf("Wake time: %v\n", wake_time)
+    fmt.Printf("Hello nurse: %d\n", now.Hour())
 }
