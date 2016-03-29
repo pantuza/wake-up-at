00de271b53ae6d45a97347d1b2bd4529844fab51
{NAME} <gustavopantuza@gmail.com>
2016-03-29 00:25:16 -0300

    Refactoring time.Date call

--- wake-up-at.go
+++ wake-up-at.go
@@ -35,13 +35,11 @@ func main () {
     if isMorning(period) {
 
         if now.Hour() > MIDDAY {
-
             wake_time = wake_time.AddDate(0, 0, 1)
         }
 
-        wake_time = time.Date(
-            wake_time.Year(), wake_time.Month(), wake_time.Day(),
-            hours, minutes, 0, 0, wake_time.Location())
+        wake_time = time.Date(wake_time.Year(), wake_time.Month(),
+            wake_time.Day(), hours, minutes, 0, 0, wake_time.Location())
     }
 
     diff := wake_time.Add(-270 * time.Minute)
