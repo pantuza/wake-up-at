0f008394c8d0587d889157d25b04c3ad25c0e984
{NAME} <gustavopantuza@gmail.com>
2016-03-28 23:31:23 -0300

    Added the main logic for time calculation

--- wake-up-at.go
+++ wake-up-at.go
@@ -14,30 +14,24 @@ func main () {
     now := time.Now()
 
     // Fake input data
-    in_period := "AM"
-    in_hours := 8
-    in_minutes := 15
+    period := "AM"
+    hours := 8
+    minutes := 15
 
 
-    // Create the time to wake
-    var hours time.Duration = time.Duration(in_hours) * time.Hour
-    var minutes time.Duration = time.Duration(in_minutes) * time.Minute
-    var wake_duration time.Duration = hours + minutes
-
-
-
-    wake_time := time.Now()
-    if in_period == "AM" {
+    wake_time := now
+    if period == "AM" {
 
         if now.Hour() > 12 {
 
-
-            fmt.Printf("Duration time: %v\n", wake_duration)
             wake_time = wake_time.AddDate(0, 0, 1)
-            //wake_time = wake_time.Truncate(wake_duration)
         }
+
+        wake_time = time.Date(
+            wake_time.Year(), wake_time.Month(), wake_time.Day(),
+            hours, minutes, 0, 0, wake_time.Location())
     }
 
-    fmt.Printf("Wake time: %v\n", wake_time)
-    fmt.Printf("Hello nurse: %d\n", now.Hour())
+    diff := wake_time.Add(-270 * time.Minute)
+    fmt.Printf("Wake time: %v\n", diff)
 }
