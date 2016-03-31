66cebe663cd27ae58877143dfb5458ac1cb7a232
{NAME} <gustavopantuza@gmail.com>
2016-03-31 00:33:42 -0300

    Created function to test Hour input option

--- parse_options_test.go
+++ parse_options_test.go
@@ -4,6 +4,8 @@ package main
 
 import (
     "testing"
+    "os"
+    "flag"
 )
 
 
@@ -21,5 +23,22 @@ func TestDefaultCommandlineOptionsValues(t *testing.T) {
         t.Fatal("Default minute must be 0, got ", minutes)
     } else if period != "AM" {
         t.Fatal("Default period must be AM, got ", period)
+    }
+}
+
+
+func TestHourInputOption(t *testing.T) {
+
+    flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+    os.Args = []string{"cmd", "-m", "10"}
+
+    var hours int
+    var minutes int
+    var period string
+
+    parseOptions(&hours, &minutes, &period)
+
+    if minutes != 10 {
+        t.Fatal("wake up minutes must be 10, got ", minutes)
     }
 }
