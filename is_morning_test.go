67e266917998558b8f3046cb3347e6ce0196f614
Zach Kipp <kipp.zach@gmail.com>
2016-10-05 12:53:25 -0600

    Fix lint warnings. Run gofmt on go source. Fix formatting directives

--- is_morning_test.go
+++ is_morning_test.go
@@ -1,33 +1,26 @@
 package main
 
-
 import (
-
-    "testing"
+	"testing"
 )
-
 
 func TestIsMorningTrue(t *testing.T) {
 
-    if !isMorning("AM") {
-        t.Fatal("Expected isMorning to true")
-    }
+	if !isMorning("AM") {
+		t.Fatal("Expected isMorning to true")
+	}
 }
-
-
 
 func TestIsMorningFalse(t *testing.T) {
 
-    if isMorning("PM") {
-        t.Fatal("Expected isMorning to false")
-    }
+	if isMorning("PM") {
+		t.Fatal("Expected isMorning to false")
+	}
 }
-
-
 
 func TestIsMorningFalseWhenDurationIsInvalid(t *testing.T) {
 
-    if isMorning("MM") {
-        t.Fatal("Expected isMorning to false")
-    }
+	if isMorning("MM") {
+		t.Fatal("Expected isMorning to false")
+	}
 }
